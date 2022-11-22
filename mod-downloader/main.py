8a6316bff03a8843947d80063b149395690f36ab
Arron Moore <arron.moore93@gmail.com>
2022-11-22 21:07:54 +0000

    Added mod downloader script

---
+++ mod-downloader/main.py
@@ -0,0 +1,23 @@
+import re
+import argparse
+
+ARMA3_APPID = 107410
+
+INSTALL_DIR = "/home/arron/arma3/gamedata"
+
+def main(args):
+    with open("./sog_mods.html") as mods_html_file:
+        mods_html = mods_html_file.read()
+
+    mod_ids = set(re.findall(r'https://.*?id=([0-9]+)', mods_html))
+    for mod_id in mod_ids:
+        print(f"steamcmd +login {args.username} \"{args.password}\" +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id}")
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", dest="username")
+    parser.add_argument("-p", dest="password")
+    args = parser.parse_args()
+    main(args)
