2b6a4d558dbe85a17bfc3f1756fecb2948f0dab0
Arron Moore <arron.moore93@gmail.com>
2022-11-22 21:11:07 +0000

    Add required to username/password

--- mod-downloader/main.py
+++ mod-downloader/main.py
@@ -11,13 +11,14 @@ def main(args):
 
     mod_ids = set(re.findall(r'https://.*?id=([0-9]+)', mods_html))
     for mod_id in mod_ids:
-        print(f"steamcmd +login {args.username} \"{args.password}\" +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id}")
+        print(f"steamcmd +login {args.username} \"{args.password}\" +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id} validate +quit")
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", dest="username")
-    parser.add_argument("-p", dest="password")
+    parser.add_argument("-u", dest="username", required=True)
+    parser.add_argument("-p", dest="password", required=True)
+
     args = parser.parse_args()
     main(args)
