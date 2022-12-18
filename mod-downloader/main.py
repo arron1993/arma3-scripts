e6f4f4fa4317a7a86df41b5615dccf7fdde886ed
Arron Moore <arron.moore93@gmail.com>
2022-12-18 00:24:21 +0000

    Addded printing mod list in server start format

--- mod-downloader/main.py
+++ mod-downloader/main.py
@@ -5,15 +5,17 @@ import subprocess
 ARMA3_APPID = 107410
 
 INSTALL_DIR = "/home/arron/arma3/gamedata"
-
+MOD_DIR_TEMPLATE = "./steamapps/workshop/content/{}/{}"
 def main(args):
-    with open("./sog_mods.html") as mods_html_file:
+    with open(args.filename) as mods_html_file:
         mods_html = mods_html_file.read()
 
     mod_ids = set(re.findall(r'https://.*?id=([0-9]+)', mods_html))
+    mod_directories = []
     for mod_id in mod_ids:
-        subprocess.run(f"steamcmd +login {args.username} {args.password} +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id} validate +quit".split())
-
+       mod_directories.append(MOD_DIR_TEMPLATE.format(ARMA3_APPID, mod_id))
+       subprocess.run(f"steamcmd +login {args.username} {args.password} +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id} validate +quit".split())
+    print(';'.join(mod_directories))
 
 
 
@@ -21,6 +23,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", dest="username", required=True)
     parser.add_argument("-p", dest="password", required=True)
-
+    parser.add_argument("-f", dest="filename", required=True)
     args = parser.parse_args()
     main(args)
