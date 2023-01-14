7718e03f1502dfd649c0ca4b66a5cbda8feea2f9
Arron Moore <arron.moore93@gmail.com>
2023-01-14 16:02:12 +0000

    Add automatic script generation after downloading mods

--- mod-downloader/main.py
+++ mod-downloader/main.py
@@ -1,11 +1,46 @@
 import re
+import os
+import stat
 import argparse
 import subprocess
+
+from pathlib import Path
 
 ARMA3_APPID = 107410
 
 INSTALL_DIR = "/home/arron/arma3/gamedata"
 MOD_DIR_TEMPLATE = "./steamapps/workshop/content/{}/{}"
+
+HC_PASSWORD = os.environ.get("HC_PASSWORD")
+
+
+def write_script(name, script):
+    if not os.path.exists("./scripts"):
+        os.makedirs("./scripts")
+    file_ = f"./scripts/{name}_start.sh"
+
+    with open(file_, 'w') as f:
+        f.write(script)
+
+    st = os.stat(file_)
+    os.chmod(file_, st.st_mode | stat.S_IEXEC)
+
+
+def generate_start_script(modset_name=None, mods=None, profile_name=None,  headless=False):
+    """
+    cd ./gamedata && ./arma3server_x64 -name="am_server_vanilla_1" -enableHT -config=./config/server.cfg -mod="./steamapps/workshop/content/107410/1858075458;./steamapps/workshop/content/107410/2158809703;./steamapps/workshop/content/107410/333310405;./steamapps/workshop/content/107410/2479270597;./steamapps/workshop/content/107410/450814997;./steamapps/workshop/content/107410/825179978;./steamapps/workshop/content/107410/1224892496;./steamapps/workshop/content/107410/1158566432;./steamapps/workshop/content/107410/861133494;./steamapps/workshop/content/107410/1858070328;./steamapps/workshop/content/107410/1862208264;./steamapps/workshop/content/107410/2912941775;./steamapps/workshop/content/107410/2261809404;./steamapps/workshop/content/107410/612930542;./steamapps/workshop/content/107410/1808238502"
+    """
+    script = ["cd ./gamedata && ./arma3server_x64 -enableHT -config=./config/server.cfg"]
+
+    if mods:
+        script.append(f'-mod="{";".join(mods)}"')
+
+    if headless:
+        script.append(f"-client -connect=game.arron.id -password={HC_PASSWORD}")
+        modset_name = f"{modset_name}_hc"
+
+
+    write_script(modset_name, ' '.join(script))
 
 
 def main(args):
@@ -16,10 +51,13 @@ def main(args):
     mod_directories = []
     for mod_id in mod_ids:
         mod_directories.append(MOD_DIR_TEMPLATE.format(ARMA3_APPID, mod_id))
-        subprocess.run(
-            f"steamcmd +login {args.username} {args.password} +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id} validate +quit".split()
-        )
-    print(";".join(mod_directories))
+        # subprocess.run(
+        #     f"steamcmd +login {args.username} {args.password} +force_install_dir {INSTALL_DIR} +workshop_download_item {ARMA3_APPID} {mod_id} validate +quit".split()
+        # )
+
+    modset_name = Path(args.filename).stem
+    generate_start_script(modset_name=modset_name, mods=mod_directories, profile_name=f"am_server_{modset_name}_profile", headless=False)
+    generate_start_script(modset_name=modset_name, mods=mod_directories, headless=True)
 
 
 if __name__ == "__main__":
